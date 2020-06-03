7be4878728f0c0e67fa08efff2ecd4667d7ff04e
Hanphantom <bloxxom4687@gmail.com>
2020-06-03 20:28:11 +0900

    2 커밋

--- Git Hub.c
+++ Git Hub.c
@@ -3,5 +3,12 @@
 int main {
 	printf("Hello World");
 	
+	int a,b;
+	scanf("%d %d", &a,&b);
+	printf("%d + %d = %d", a,b,a+b);
+	printf("%d - %d = %d", a,b,a-b);
+	printf("%d * %d = %d", a,b,a*b);
+	printf("%d / %d = %d", a,b,a/b);
+
 	return 0;
 }
